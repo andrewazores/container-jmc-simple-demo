3aeaae96e502ddc18397e66ae82c025c820fb80f
Andrew Azores <me@andrewazor.es>
2019-02-26 15:38:13 -0500

    Initial commit
    Move Listener and various build and demo files from JMXClient repository
    to this one

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [ -z "$CMD" ]; then
+    CMD="$(command -v podman || command -v docker)"
+fi
+
+./gradlew clean build
+$CMD build -f Dockerfile.docker.listener -t andrewazores/container-jmx-docker-listener:latest .
+$CMD build -f Dockerfile.podman.listener -t andrewazores/container-jmx-podman-listener:latest .
+$CMD push andrewazores/container-jmx-docker-listener
+$CMD push andrewazores/container-jmx-podman-listener
